36da57790a31989f6f9358d9c6d8039a5fc772df
Maxmarra <5919669@gmail.com>
2022-01-02 18:19:35 +0300

    add dependencies

--- app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
@@ -1,11 +1,12 @@
-package com.example.podplay
+package com.example.podplay.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.podplay.R
 
-class MainActivity : AppCompatActivity() {
+class PodcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_podcast)
     }
 }
